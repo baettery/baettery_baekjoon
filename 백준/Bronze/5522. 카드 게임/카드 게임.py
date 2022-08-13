871635480109d49136f01a5eeff12c819fdf9d8a
baettery <107979677+baettery@users.noreply.github.com>
2022-08-13 23:39:05 +0900

    [Bronze V] Title: 카드 게임, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.py
@@ -0,0 +1,7 @@
+import sys
+a1 = int(sys.stdin.readline())
+a2 = int(sys.stdin.readline())
+a3 = int(sys.stdin.readline())
+a4 = int(sys.stdin.readline())
+a5 = int(sys.stdin.readline())
+print(a1+a2+a3+a4+a5)
