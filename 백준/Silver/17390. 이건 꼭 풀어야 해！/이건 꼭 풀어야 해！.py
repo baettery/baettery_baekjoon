a1c37d7474a36b6518ffb68d7213cd25eabca7b6
baettery <107979677+baettery@users.noreply.github.com>
2022-08-26 00:07:00 +0900

    [Silver III] Title: 이건 꼭 풀어야 해!, Time: 756 ms, Memory: 63192 KB -BaekjoonHub

---
+++ 백준/Silver/17390. 이건 꼭 풀어야 해！/이건 꼭 풀어야 해！.py
@@ -0,0 +1,10 @@
+import sys
+n, m = map(int, sys.stdin.readline().split())
+k = [0]
+k += list(map(int,sys.stdin.readline().split()))
+k.sort()
+for i in range(1, n+1):
+    k[i] += k[i-1]
+for i in range(m):
+    a, b = map(int,sys.stdin.readline().split())
+    print(k[b]-k[a-1])
