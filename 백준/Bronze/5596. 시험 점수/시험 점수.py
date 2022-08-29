51db94dbcabb82d0f099538e21dca94b4063c5e2
baettery <107979677+baettery@users.noreply.github.com>
2022-08-30 04:54:45 +0900

    [Bronze IV] Title: 시험 점수, Time: 80 ms, Memory: 43456 KB -BaekjoonHub

---
+++ 백준/Bronze/5596. 시험 점수/시험 점수.py
@@ -0,0 +1,11 @@
+from hashlib import sha512
+import sys
+input = sys.stdin.readline
+L1 = list(map(int,input().split()))
+L2 = list(map(int,input().split()))
+s1 = sum(L1)
+s2 = sum(L2)
+if s1>=s2:
+    print(s1)
+else:
+    print(s2)
